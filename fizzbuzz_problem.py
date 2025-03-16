fe0e6b60ef5092215b42103aff71f42a642034c6
LBABO1 <mafantiribabongile@gmail.com>
2025-03-16 11:03:07 +0200

    Update fizzbuzz_problem.py

--- fizzbuzz_problem.py
+++ fizzbuzz_problem.py
@@ -1,5 +1,5 @@
 for i in range(1,101):
-    if i%3 == 0 and i%5 == 0:
+    if i%3 == 0 and i%5 == 0: #It's imperative that this line of code comes first. Otherwise, it's only going to print Fizz or Buzz even for numbers divisible by both 3 and 5.
        print("FizzBuzz")
     
     if i%3 == 0:
